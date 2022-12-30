eb258d4b2ff005c6a52c3eff3a70215e253a0018
AndreiGoriunov <52248218+AndreiGoriunov@users.noreply.github.com>
2022-12-29 21:06:11 -0500

    Add Soundboard page Directory restructure

---
+++ assets/javascript/soundboard.js
@@ -0,0 +1,33 @@
+const audioFiles = ["aughh.mp3", "lyagushka.mp3", "woo.mp3", "woo.mp3", "woo.mp3"];
+
+
+function createSoundButtons() {
+    let soundboardContainer = document.getElementById("soundboard-container");
+
+    const fragment = new DocumentFragment();
+    const soundButtonsArray = [];
+
+    audioFiles.forEach(fileName => {
+        const fileExtension = fileName.split('.').pop();
+        const filePath = `/assets/audio/soundboard/${fileName}`
+        const soundButtonContentsTemplate = `
+            <p class="sound-button__name">${fileName}</p>
+            <audio src="${filePath}" type="audio/${fileExtension}" ></audio>
+            `;
+        const soundButton = document.createElement("button");
+        soundButton.classList.add("soundboard-container__sound-button");
+        soundButton.innerHTML = soundButtonContentsTemplate;
+        // addEventListener
+        soundButton.addEventListener("click", () => {
+            const audio = soundButton.getElementsByTagName("audio")[0];
+            audio.play();
+        });
+        soundButtonsArray.push(soundButton);
+        fragment.appendChild(soundButton);
+    });
+    // Add buttons to DOM
+    soundboardContainer.appendChild(fragment);
+}
+
+createSoundButtons();
+
